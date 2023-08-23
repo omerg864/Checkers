ce95262cdd977660f2fb6af412309123214620f4
אסף עמוסי <assafamosi@gmail.com>
2023-08-23 16:25:13 +0300

    updated

--- HomePage.js
+++ HomePage.js
@@ -5,30 +5,41 @@ document.addEventListener("DOMContentLoaded", function () {
     const playerNameInput2 = document.getElementById("player-name-2");
     const selectButton = document.getElementById("select-button");
     const selectedTeamsDisplay = document.getElementById("selected-teams");
-
-    selectButton.addEventListener("click", function () {
+const title =document.getElementById('exampleModalLabel')
+    const message =document.getElementById('messageContent');
+    document.getElementById("test").onclick=function () {
         const teamColor1 = teamColorSelect1.value;
         const playerName1 = playerNameInput1.value;
         const teamColor2 = teamColorSelect2.value;
         const playerName2 = playerNameInput2.value;
 
+
         if (playerName1.trim() === "" || playerName2.trim() === "") {
-            alert("Please enter names for both players.");
-            return;
+            message.textContent ='Please enter names for both players.';
+            title.innerText="ERROR";
+
+
         }
-        if(teamColor1===teamColor2)
+        else
+            if(teamColor1===teamColor2)
         {
-            alert("Cannot select the same team");
+
+            message.textContent ='choose differnt teams';
+            title.innerText="ERROR";
+
         }
+
         else {
+                title.innerText="The teams are";
 
-
-            selectedTeamsDisplay.textContent = `
+            message.textContent = `
             Player 1: ${playerName1} (Team ${teamColor1}) 
             vs. 
             Player 2: ${playerName2} (Team ${teamColor2})
             
+            
         `;
         }
-    });
+
+    }
 });
